61ac1b2557f502157da41c2451039a9a1820fe7e
Chris Yungmann <yungmann.chris@gmail.com>
2014-04-25 22:28:39 -0500

    add getTermCourses, getCourses, etc
    and add index.html

--- js/app.js
+++ js/app.js
@@ -3,16 +3,61 @@
     Caesar = (function(){
       var base = 'http://vazzak2.ci.northwestern.edu/';
       Caesar.getSubjects = function(cb){
-        return $.get(base + 'subjects').done(function(data, textStatus, jqXHR){
-          return cb(undefined, data);
+        $.get(base + 'subjects').done(function(data, textStatus, jqXHR){
+          cb(undefined, data);
         }).fail(function(jqXHR, textStatus, err){
-          return cb(err, undefined);
+          cb(err, undefined);
         });
       };
+      Caesar.getTerms = function(cb){
+        $.get(base + 'terms').done(function(data, textStatus, jqXHR){
+          cb(undefined, data);
+        }).fail(function(jqXHR, textStatus, err){
+          cb(err, undefined);
+        });
+      };
+      Caesar.getCourses = function(term, subject, cb){
+        $.get(base + 'courses/?term=' + term + '&subject=' + subject).done(function(data, textStatus, jqXHR){
+          cb(undefined, data);
+        }).fail(function(jqXHR, textStatus, err){
+          cb(err, undefined);
+        });
+      };
+      Caesar.getTermCourses = function(term, cb){
+        var termCourses = [];
+        var counter = 0;
+        Caesar.getSubjects(function(err, subjects) {
+          if (err)
+            return cb(err, undefined);
+          $.each(subjects, function(indexInArray, valueOfElement) {
+            Caesar.getCourses(term, valueOfElement.symbol, function(err, courses) {
+              if (err)
+                return cb(err, undefined);
+              termCourses = termCourses.concat(courses);
+              counter += 1;
+              if (counter == subjects.length - 1)
+                cb(undefined, termCourses);
+            });
+          });
+        });
+      }
       function Caesar(){}
       return Caesar;
     }());
-    Caesar.getSubjects(function(err, subjects) {
+
+    /*Caesar.getSubjects(function(err, subjects) {
       console.log(subjects);
+    });*/
+    Caesar.getCourses('4530', 'EECS', function(err, courses) {
+      var filteredCourses;
+    filteredCourses = $.grep(courses, function(course) {
+      return course.seats > 50;
+    });
+    console.log("filtered courses:");
+    console.log(filteredCourses);
+    });
+    Caesar.getTermCourses('4530', function(err, termCourses) {
+      console.log("term courses:");
+      console.log(termCourses);
     });
 }).call(this, window, window.document, window.jQuery);
