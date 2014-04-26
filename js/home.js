848f689df3f63866f5e2c5c4270a9f06136402a8
thomasythuang <thomas.yt.huang@gmail.com>
2014-04-26 14:08:34 -0500

    resize/reformat calendar

--- js/home.js
+++ js/home.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
-    scheduler.config.first_hour = 8;
-    scheduler.config.last_hour = 26;
+    scheduler.config.first_hour = 7;
+    scheduler.config.last_hour = 24;
     scheduler.config.start_on_monday = false;
     scheduler.config.dblclick_create = false;
     scheduler.config.mark_now = true;
