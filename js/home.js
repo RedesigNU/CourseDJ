5cf3ce5d0aebcfdfce461a2c0bb65a9e0315ba7b
thomasythuang <thomas.yt.huang@gmail.com>
2014-04-26 02:00:40 -0500

    minor styling changes

--- js/home.js
+++ js/home.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
     scheduler.config.first_hour = 8;
     scheduler.config.last_hour = 26;
     scheduler.config.start_on_monday = false;
+    scheduler.config.mark_now = true;
     scheduler.init('scheduler_here', new Date(), "week"); //Set for 1st week of qtr here
     
     var sample = [
